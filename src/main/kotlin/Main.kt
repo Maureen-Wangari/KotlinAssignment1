d538659634437f2427944486977d7bc114fb0431
maureen <maureenwangarinjeri@gmail.com>
2024-02-08 07:03:34 +0300

    added to asignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var name = "Maureen"
+    name = "Maureen"
+    println(name)
+    var maureenAge: Byte = 23
+    println(maureenAge)
+    var maureenContact: String = "072435887899"
+    println(maureenContact)
+    var maureenWeight: Long = 65
+    println(maureenWeight)
+    var maureenKenyanStatus: Boolean = true
+    println(maureenKenyanStatus)
+}
